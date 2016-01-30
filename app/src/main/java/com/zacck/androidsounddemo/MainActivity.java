118d5f2fe3766ea866ed9d76f5ca4669c54a5c77
zacck <coderv63@gmail.com>
2016-01-30 09:03:37 +0200

    Added buttons on the UI to use as controllers for the media player

--- app/src/main/java/com/zacck/androidsounddemo/MainActivity.java
+++ app/src/main/java/com/zacck/androidsounddemo/MainActivity.java
@@ -2,12 +2,35 @@ package com.zacck.androidsounddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare it in  a public scope
+    MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //make a mediaplayer instance with the file
+        mPlayer = MediaPlayer.create(this, R.raw.cheering);
+
+
     }
+
+    public void audioPlay(View view)
+    {
+        mPlayer.start();
+    }
+
+    public void audioPause(View view)
+    {
+        mPlayer.stop();
+    }
+
+
 }
